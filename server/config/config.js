c3b6647d6154e12e258203b4f00db24f3935034f
gaurav gupta <gaurav_bng@hotmail.com>
2016-05-19 10:49:19 +0530

    initial commit with port change to 6000

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
     server: {
             host: '0.0.0.0',
-            port: 3000
+            port: 6000
     },
     database: {
         host: '127.0.0.1',
         port: 27017,
-        db: 'Leader',
+        db: 'hillBikeRacing',
         username: '',
         password: ''
     },
