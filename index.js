a2f6f3cf775f0789c1467431a23d85c2aa88d265
Amrendra Singh <amrendra.singh@w3villa.com>
2017-02-17 12:27:26 +0530

    Added package json

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ var messageData = {
       contentAvailable: true,
       delayWhileIdle: true,
       data: {
-        title: "Creatiosoft",         // this will be the title in push sent
-        subtitle: "Only for games",   // this will be the subtitle in push sent
-        icon: "ic_launcher",
-        body: "notification body"
-        company: 'Creatiosoft',
-        location: 'Noida'
+        title    :  "Creatiosoft",         // this will be the title in push sent
+        subtitle :  "Only for games",   // this will be the subtitle in push sent
+        icon     :  "ic_launcher",
+        body     :  "notification body",
+        company  :  'Creatiosoft',
+        location :  'Noida'
       }
 }
 
